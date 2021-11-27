434a11f6c9c2cc2c9191073b6158e6a6692c5a5d
Sergio <sergio.ivan.melgarejo@gmail.com>
2021-11-27 12:27:24 -0300

    i finished the search and filter for region

--- script.js
+++ script.js
@@ -4,14 +4,18 @@
 const body = document.getElementById("body"),
     buttonDark = document.getElementById("buttonDark"),
     navSelect = document.getElementById("navSelect"),
+    navRegion = document.getElementById("navRegion"),
     flagsContainer = document.getElementById("flagsContainer"),
     loader = document.getElementById("loader"),
     main = document.getElementById("main"),
     backButton = document.getElementById("backButton"),
     infoFlag = document.getElementById("infoFlag"),
+    navSearch = document.getElementById("search"),
     ContainerBorderCountries = document.getElementById("ContainerBorderCountries");
 let dataFlags,
-    memorize;
+    memorizeFlag,
+    memorizeRegion = document.querySelector(".nav__li-0"),
+    dataFlagsFilter;
 
 // * Function
 
@@ -46,11 +50,11 @@ const createFlag = obj => {
     img.setAttribute("alt", `${obj.name} flag`);
     h2.textContent = obj.name;
     p.innerHTML = `
-                <p class="flag__p">
                     Population: <span class="flag__span">${obj.population}</span><br>
                     Region: <span class="flag__span">${obj.region}</span><br>
                     Capital: <span class="flag__span">${obj.capital}</span><br>
-                </p>`;
+                `;
+    flag.setAttribute("data-region", obj.region);
 
     flag.appendChild(img);
     div.appendChild(h2);
@@ -66,13 +70,15 @@ const loaderToggle = () => {
     loader.classList.toggle("open");
 }
 
-const loadFlags = async () => {
+const loadFlags = async (  ) => {
     dataFlags = await getData();
+    
     let fragment = document.createDocumentFragment();
     dataFlags.forEach( element => {
         let obj = {},
             name;
 
+        obj.region = element.region;
         obj.img = element.flags.png;
         obj.region = element.region;
 
@@ -91,7 +97,7 @@ const loadFlags = async () => {
         fragment.appendChild( createFlag(obj) );
     });
     flagsContainer.appendChild(fragment);
-console.log(dataFlags)
+
     loaderToggle();
 }
 
@@ -100,7 +106,7 @@ const selecFlag = (element) => {
 
     let name = element.getAttribute("data-name");
 
-    if ( memorize !== name ) {  
+    if ( memorizeFlag !== name ) {  
         
         fetch(`https://restcountries.com/v2/name/${name}`)
             .then( res => {
@@ -144,7 +150,15 @@ const selecFlag = (element) => {
                     }
                     insertBorderCountries(countries);
                 }
-                else{
+                else {
+                    ContainerBorderCountries.removeChild( ContainerBorderCountries.children[1] );
+                     
+                    const spanContainer = document.createElement("span");
+                    spanContainer.classList.add("box-2__country");
+                    spanContainer.id = "borderCountries";
+
+                    ContainerBorderCountries.appendChild(spanContainer);
+
                     const span = document.createElement("span");
                     span.classList.add("box-2__a");
                     span.textContent = "none";
@@ -193,15 +207,64 @@ const insertBorderCountries = async (array) => {
     }
 }
 
+const filter = () => {
+    let region = navRegion.textContent;
+    let search = navSearch.value.toLocaleLowerCase();
+
+    // Region
+    if ( region == "America" ) region = "Americas"; 
+
+    if ( region == "Filter by Region" ) {
+        for ( let i = 0; i < flagsContainer.children.length; i++ ) {
+            flagsContainer.children[i].classList.remove("filter");
+        }
+    }
+    else {
+        for ( let i = 0; i < flagsContainer.children.length; i++ ) {
+            ( flagsContainer.children[i].getAttribute("data-region") == region ) ? flagsContainer.children[i].classList.remove("filter") : flagsContainer.children[i].classList.add("filter")
+        }
+    }
+
+    // Search
+    for ( let i = 0; i < flagsContainer.children.length; i++ ){
+        let flag = flagsContainer.children[i];
+        if ( !flag.classList.contains("filter") ){
+            (flag.children[2].getAttribute("data-name").toLocaleLowerCase().includes( search )) ? flag.classList.remove("filter") : flag.classList.add("filter");
+        }
+    }
+}
+
+// modo dark?
+const localstorage = () => {
+    let dark = localStorage.getItem("dark-mode");
+    if ( dark ){
+        body.classList.add("dark");
+        buttonDark.children[1].textContent = "Light Mode";
+        buttonDark.children[0].classList.remove("fa-moon");
+        buttonDark.children[0].classList.add("fa-sun");
+    }
+}
+
 // * Event
 
 addEventListener("DOMContentLoaded", () => {
     loadFlags();
 
-    navSelect.addEventListener("click", () => {
-        navSelect.children[0].classList.toggle("open");
-        navSelect.children[1].classList.toggle("open");
-        navSelect.children[2].classList.toggle("open");
+    navSelect.addEventListener("click", (e) => {
+        if ( !e.target.classList.contains("nav__ul") ) {
+            navSelect.children[0].classList.toggle("open");
+            navSelect.children[1].classList.toggle("open");
+            navSelect.children[2].classList.toggle("open");
+
+            if ( e.target.classList.contains("nav__li") ) {
+                e.target.classList.add("select");
+                memorizeRegion.classList.remove("select");
+                navRegion.textContent = e.target.textContent;
+                memorizeRegion = e.target;
+
+                filter();
+            }
+        }
     })
 
     flagsContainer.addEventListener("click", (e) => {
@@ -214,6 +277,22 @@ addEventListener("DOMContentLoaded", () => {
             selecFlag( e.target );
         }
     })
+
+    backButton.addEventListener("click", () => {
+        main.children[0].classList.add("open");
+        main.children[1].classList.remove("open");
+    })
+    
+    ContainerBorderCountries.addEventListener("click", (e) => {
+        if ( e.target.classList.contains("box-2__a") && e.target.textContent != "none" ) {
+            selecFlag(e.target);
+        }
+    })
+
+    navSearch.addEventListener("keyup", (e) => {
+        filter();
+    })
+    
 })
 
 buttonDark.addEventListener("click", () => {
@@ -222,22 +301,18 @@ buttonDark.addEventListener("click", () => {
         buttonDark.children[1].textContent = "Dark Mode";
         buttonDark.children[0].classList.remove("fa-sun");
         buttonDark.children[0].classList.add("fa-moon");
+
+        localStorage.removeItem("dark-mode");
     }
     else {
         body.classList.add("dark");
         buttonDark.children[1].textContent = "Light Mode";
         buttonDark.children[0].classList.remove("fa-moon");
         buttonDark.children[0].classList.add("fa-sun");
+
+        localStorage.setItem("dark-mode", true);
     }
 })
 
-backButton.addEventListener("click", () => {
-    main.children[0].classList.add("open");
-    main.children[1].classList.remove("open");
-})
-
-ContainerBorderCountries.addEventListener("click", (e) => {
-    if ( e.target.classList.contains("box-2__a") ) {
-        selecFlag(e.target);
-    }
-})
+// self-execute
+localstorage()
